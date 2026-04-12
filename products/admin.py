f08fb7a3133f42e27fe14301fa37ba8df622ec0d
swager2122se <ajitesh01420@gmail.com>
2026-04-12 16:03:43 -0700

    Added products with models and admin registration

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Tag, Product
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Product)
+# Register your models here.    
--- products/models.py
+++ products/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.title
